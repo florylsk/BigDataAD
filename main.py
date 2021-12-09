19a70b794bae228a8ce4673c24cabcdcaaac9295
florylsk <florilsk@yahoo.es>
2021-12-09 10:57:05 +0100

    Plots

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ import gc
 from collections import Counter
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from textblob import TextBlob
 
 
 def main():
@@ -112,6 +124,92 @@ def main():
         plt.show()
 
 
+    def analize_data(dataframe):
+        tmpdf = dataframe.drop(['business_id_x', 'user_id', 'review_id', 'business_id_y'], axis=1);del dataframe
+        states=tmpdf.state.unique()
+        # sns.catplot(x="state", y="rating_y", data=tmpdf, height=50)
+        # plt.show()
+        plt.figure(figsize=(16, 6))
+        ax = sns.barplot(x='state', y='rating_y',data=tmpdf)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right", fontsize=8)
+        plt.title('Distribution of rating per state', fontsize=14, fontweight='bold')
+        plt.show()
+        # X_cat=tmpdf['categories']
+        # X_cat=pd.get_dummies(data=X_cat,drop_first=True)
+        # # X_cat.replace(to_replace=pd.NA, value=None, inplace=True)
+        # y_cat=tmpdf['rating_y']
+        # # y_cat.replace(to_replace=pd.NA, value=None, inplace=True)
+        # # del tmpdf
+        # X_train_, X_test_, y_train_, y_test_ = train_test_split(X_cat, y_cat, test_size=0.3, random_state=1)
+        # # training the algorythm
+        #
+        # lr_eval_ = LinearRegression()
+        # lr_eval_.fit(X_train_, y_train_)
+        # # making predictions
+        # y_pred_ = lr_eval_.predict(X_test_)
+        # df_eval_ = pd.DataFrame({'Actual': y_test_, 'Predicted': y_pred_})
+        # print(df_eval_)
+        # # calculating the model metrics
+        #
+        # rmse_eval_ = np.sqrt(metrics.mean_squared_error(y_test_, y_pred_))
+        # r2_eval_ = metrics.mean_squared_error(y_test_, y_pred_)
+        # print('Mean Absolute Error:', metrics.mean_absolute_error(y_test_, y_pred_))
+        # print('Mean Squared Error:', r2_eval_)
+        # print('Root Mean Squared Error:', rmse_eval_)
+
+    def sentiment_analysis(df):
+        tmpdf = df.drop(['business_id_x', 'user_id', 'review_id', 'business_id_y'], axis=1);del df
+        #use business "Be Epic Athletics"
+        tmpdf=tmpdf[tmpdf.state=='NC']
+        business_polarity = pd.DataFrame(columns=['Business','Polarity'])
+        for business in tmpdf['name'].unique():
+            businessDF=tmpdf[tmpdf.name==business]
+            unique_reviews = businessDF['description'].unique()
+            total_polarity = 0
+            for review in unique_reviews:
+                tmpBlob = TextBlob(review)
+                total_polarity += tmpBlob.polarity
+            relative_polarity = total_polarity / len(unique_reviews)
+            business_polarity=business_polarity.append({"Business": business, "Polarity": relative_polarity}, ignore_index=True)
+
+        #Most positive reviews
+        business_polarity=business_polarity.sort_values(by='Polarity')
+        polarity_values=business_polarity['Polarity'].head(12)
+        business_names=business_polarity['Business'].head(12)
+        fig, ax = plt.subplots(figsize=(16, 9))
+        ax.barh(business_names, polarity_values)
+        for s in ['top', 'bottom', 'left', 'right']:
+            ax.spines[s].set_visible(False)
+        ax.xaxis.set_ticks_position('none')
+        ax.yaxis.set_ticks_position('none')
+        ax.xaxis.set_tick_params(pad=5)
+        ax.yaxis.set_tick_params(pad=10)
+        ax.grid(visible=True, color='grey',
+                linestyle='-.', linewidth=0.5,
+                alpha=0.2)
+        ax.invert_yaxis()
+        ax.set_title('Top negative reviewed business',
+                     loc='left', )
+        plt.show()
+        #Most negative reviews
+        business_polarity = business_polarity.sort_values(by='Polarity', ascending=False)
+        polarity_values = business_polarity['Polarity'].head(12)
+        business_names = business_polarity['Business'].head(12)
+        fig, ax = plt.subplots(figsize=(16, 9))
+        ax.barh(business_names, polarity_values)
+        for s in ['top', 'bottom', 'left', 'right']:
+            ax.spines[s].set_visible(False)
+        ax.xaxis.set_ticks_position('none')
+        ax.yaxis.set_ticks_position('none')
+        ax.xaxis.set_tick_params(pad=5)
+        ax.yaxis.set_tick_params(pad=10)
+        ax.grid(visible=True, color='grey',
+                linestyle='-.', linewidth=0.5,
+                alpha=0.2)
+        ax.invert_yaxis()
+        ax.set_title('Top positive reviewed business',
+                     loc='left', )
+        plt.show()
 
     def concurrent_downloads(filenames):
         # choose the number of threads
@@ -150,7 +248,9 @@ def main():
     gc.collect()
     print("100%")
     print("dataframe fully merged")
-    pregunta_1(df_review_user_business_check_advice)
+    #pregunta_1(df_review_user_business_check_advice)
+    #analize_data(df_review_user_business_check_advice)
+    sentiment_analysis(df_review_user_business_check_advice)
     del df_review_user_business_check_advice
     end = time.time()
     print("Time elapsed:", end - start, " seconds")
